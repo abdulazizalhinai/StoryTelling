542ed266800330e235a23dd4de7a7d996cb270b1
abdulazizalhinai <47962369+abdulazizalhinai@users.noreply.github.com>
2019-04-10 09:15:16 +0400

    updated

---
+++ StoryTelling/Assets/Vuforia/Scripts/Test.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : DefaultTrackableEventHandler
+{
+  
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    protected override void OnTrackingFound()
+    {
+        transform.position = new Vector3(transform.position.x, transform.position.y+0.1f, transform.position.z);
+        base.OnTrackingFound();
+    }
+}
