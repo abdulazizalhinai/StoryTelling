14c0f40ded78e2457b3614713273665ddb43ed45
abdulazizalhinai <47962369+abdulazizalhinai@users.noreply.github.com>
2019-04-11 14:36:31 +0400

    updated

--- StoryTelling/Assets/Vuforia/Scripts/Test.cs
+++ StoryTelling/Assets/Vuforia/Scripts/Test.cs
@@ -1,19 +1,53 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class Test : DefaultTrackableEventHandler
+public class Test : DefaultTrackableEventHandler, IDropHandler
 {
-  
-
+    bool move=false;
+   public Transform cube;
+  public Rigidbody rb;
     // Update is called once per frame
+    protected override void Start()
+    {
+        base.Start();
+        cube = GetComponent<Transform>();
+        rb = GetComponent<Rigidbody>();
+    }
     void Update()
     {
         
+        if (move==true)
+        {
+           // rb.velocity = 100 * transform.up;
+            //cube.transform.position = new Vector3(cube.transform.position.x + 1f, cube.transform.position.y + 1f, cube.transform.position.z + 1f);
+            print("aaaa");
+        }
     }
     protected override void OnTrackingFound()
     {
-        transform.position = new Vector3(transform.position.x, transform.position.y+0.1f, transform.position.z);
         base.OnTrackingFound();
+       // print("aaaa");
+        move = true;
+    }
+    protected override void OnTrackingLost()
+    {
+       // print("bbbb");
+        base.OnTrackingLost();
+    }
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        cube.position = Input.mousePosition;
+       
+       
+    }
+    private void OnMouseDrag()
+    {
+        cube.transform. position = Input.mousePosition;
     }
 }
+
+
+
