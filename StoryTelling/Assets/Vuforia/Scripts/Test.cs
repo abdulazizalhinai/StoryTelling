622207111925eed828f2c330f45b249a05cc604a
abdulazizalhinai <47962369+abdulazizalhinai@users.noreply.github.com>
2019-04-13 15:46:42 +0400

    updated

--- StoryTelling/Assets/Vuforia/Scripts/Test.cs
+++ StoryTelling/Assets/Vuforia/Scripts/Test.cs
@@ -3,27 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class Test : DefaultTrackableEventHandler, IDropHandler
+public class Test : DefaultTrackableEventHandler
 {
     bool move=false;
-   public Transform cube;
-  public Rigidbody rb;
+   //public Transform cube;
+  //public Rigidbody rb;
+    Transform mytransform;
+    private Vector3 mOffset;
+    private float mZCoord;
     // Update is called once per frame
     protected override void Start()
     {
         base.Start();
-        cube = GetComponent<Transform>();
-        rb = GetComponent<Rigidbody>();
+      //  cube = GetComponent<Transform>();
+        //rb = GetComponent<Rigidbody>();
     }
     void Update()
     {
         
-        if (move==true)
-        {
-           // rb.velocity = 100 * transform.up;
-            //cube.transform.position = new Vector3(cube.transform.position.x + 1f, cube.transform.position.y + 1f, cube.transform.position.z + 1f);
-            print("aaaa");
-        }
+       
     }
     protected override void OnTrackingFound()
     {
@@ -36,17 +34,56 @@ public class Test : DefaultTrackableEventHandler, IDropHandler
        // print("bbbb");
         base.OnTrackingLost();
     }
+    void OnMouseDown()
 
-    public void OnDrop(PointerEventData eventData)
     {
-        cube.position = Input.mousePosition;
-       
-       
+
+        mZCoord = Camera.main.WorldToScreenPoint(
+
+            gameObject.transform.position).z;
+
+
+
+        // Store offset = gameobject world pos - mouse world pos
+
+        mOffset = gameObject.transform.position - GetMouseAsWorldPoint();
+
     }
-    private void OnMouseDrag()
+
+
+
+    private Vector3 GetMouseAsWorldPoint()
+
     {
-        cube.transform. position = Input.mousePosition;
+
+        // Pixel coordinates of mouse (x,y)
+
+        Vector3 mousePoint = Input.mousePosition;
+
+
+
+        // z coordinate of game object on screen
+
+        mousePoint.z = mZCoord;
+
+
+
+        // Convert it to world points
+
+        return Camera.main.ScreenToWorldPoint(mousePoint);
+
     }
+
+
+
+    void OnMouseDrag()
+
+    {
+
+        transform.position = GetMouseAsWorldPoint() + mOffset;
+
+    }
+
 }
 
 
