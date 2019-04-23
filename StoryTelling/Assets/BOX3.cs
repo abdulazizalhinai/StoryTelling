b45d8ed81254817b34a1b4c2b2d115d5f22e4f22
sawsanalrashdi <47941229+sawsanalrashdi@users.noreply.github.com>
2019-04-23 11:52:20 +0400

    updated

---
+++ StoryTelling/Assets/BOX3.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BOX3 : MonoBehaviour
+{
+    public AudioSource CURRECT;
+    public AudioSource RONG;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void ANSER()
+    {
+        if (PlayerPrefs.GetInt("BOX3", 0) == 1)
+        {
+            CURRECT.Play();
+        }
+        else
+        {
+            RONG.Play();
+        }
+
+
+    }
+    public void CORECTANSER()
+    {
+
+
+        CURRECT.Play();
+
+
+
+
+
+    }
+
+    public void RONGANSER()
+    {
+        RONG.Play();
+
+
+    }
+}
---
+++ StoryTelling/Assets/ClickableObject.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickableObject : MonoBehaviour
+{
+
+    public int Answer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        Animals.Instance.CheckAnswer(Answer);
+    }
+}
